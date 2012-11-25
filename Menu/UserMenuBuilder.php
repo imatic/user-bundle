3fbbe25d4a41b88322c6ef420d5abf45850a5549
Stepan Koci <stepan.koci@imatic.cz>
2012-11-25 18:54:19 +0100

    Menu, templates, translations, styles

--- Menu/UserMenuBuilder.php
+++ Menu/UserMenuBuilder.php
@@ -7,7 +7,6 @@ use Imatic\Bundle\ViewBundle\Menu\Helper;
 
 class UserMenuBuilder
 {
-
     /**
      * @param Factory $factory
      * @param Helper $helper
@@ -15,22 +14,28 @@ class UserMenuBuilder
      */
     public function getMenu(Factory $factory, Helper $helper)
     {
-//        if ($helper->isUserLogged()) {
-////            $menu = $factory->createItem();
-//
-//            $userMenu = $helper->createDropdown($menu, $this->securityContext->getToken()->getUser());
-//            $userMenu->addChild($helper->trans('User Profile', array(), 'ImaticUserBundle'), array('route' => 'fos_user_profile_show'));
-//            $userMenu->addChild($helper->trans('Change Password', array(), 'ImaticUserBundle'), array('route' => 'fos_user_change_password'));
-//            $helper->addDivider($userMenu);
-//            if ($helper->isUserGranted('ROLE_PREVIOUS_ADMIN')) {
-//                $userMenu->addChild($helper->trans('Switch user exit', array(), 'ImaticUserBundleUser'), array('route' => 'homepage', 'routeParameters' => array('_switch_user' => '_exit')));
-//            }
-//            $userMenu->addChild($helper->trans('layout.logout', array(), 'FOSUserBundle'), array('route' => 'fos_user_security_logout'));
-//        } else {
-//            $menu = $factory->createItem($helper->trans('layout.login', array(), 'FOSUserBundle'), array('route' => 'fos_user_security_login'));
-//        }
+        $menu = $factory->createItem($helper->getUser());
+        $helper->setDropdown($menu);
+        $menu->addChild($helper->trans('User Profile', array(), 'ImaticUserBundle'), array('route' => 'fos_user_profile_show'));
+        $menu->addChild($helper->trans('Change Password', array(), 'ImaticUserBundle'), array('route' => 'fos_user_change_password'));
+        $helper->addDivider($menu);
+        if ($helper->isUserGranted('ROLE_PREVIOUS_ADMIN')) {
+            $menu->addChild($helper->trans('Switch user exit', array(), 'ImaticUserBundleUser'), array('route' => 'homepage', 'routeParameters' => array('_switch_user' => '_exit')));
+        }
+        $menu->addChild($helper->trans('layout.logout', array(), 'FOSUserBundle'), array('route' => 'fos_user_security_logout'));
 
+        return $menu;
+    }
+
+    /**
+     * @param Factory $factory
+     * @param Helper $helper
+     * @return \Knp\Menu\ItemInterface
+     */
+    public function getMenuAnon(Factory $factory, Helper $helper)
+    {
         $menu = $factory->createItem($helper->trans('layout.login', array(), 'FOSUserBundle'), array('route' => 'fos_user_security_login'));
+
         return $menu;
     }
 }
