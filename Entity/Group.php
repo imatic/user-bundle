4a2dd2c7973931b91a1eb91d5cfffb51bef98d53
Viliam Husár <viliam.husar@imatic.cz>
2013-08-28 10:10:54 +0200

    RESEARCH-12 - Úprava entit s použitím Interface

--- Entity/Group.php
+++ Entity/Group.php
@@ -1,71 +1,172 @@
 <?php
 
-namespace Imatic\Bundle\UserBundle\Entity;
+namespace Imatic\Bundle\UserBundle\Model;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints as DoctrineAssert;
 use Symfony\Component\Validator\Constraints as Assert;
-use FOS\UserBundle\Entity\Group as BaseGroup;
 
 /**
- * Imatic\Bundle\UserBundle\Entity\Group
+ * Group.
  *
- * @ORM\MappedSuperclass
- * @DoctrineAssert\UniqueEntity("name")
- * @DoctrineAssert\UniqueEntity("description")
+ * @author Viliam Husár <viliam.husar@imatic.cz>
+ *
+ * @ORM\MappedSuperclass()
+ * @DoctrineAssert\Unique(fields="usernameCanonical", errorPath="username", message="fos_user.username.already_used",  groups={"Registration", "Profile"})
+ * @DoctrineAssert\Unique(fields="emailCanonical", errorPath="email", message="fos_user.email.already_used",  groups={"Registration", "Profile"})
  */
-abstract class Group extends BaseGroup
+class Group implements GroupInterface
 {
     /**
-     * @var integer $id
+     * @var int
      *
-     * @ORM\Id
+     * @ORM\Id()
      * @ORM\Column(type="integer")
-     * @ORM\GeneratedValue()
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
 
     /**
-     * @var string $description
+     * @var string
      *
-     * @ORM\Column(type="string", length=255, unique=true, nullable=true)
-     * @Assert\Length(max=255)
+     * @ORM\Column(type="integer", unique="true")
+     * @Assert\NotBlank(message="fos_user.group.blank", groups={"Registration"})
+     * @Assert\Length(min=2, max=255, minMessage="fos_user.group.short", maxMessage="fos_user.group.long",  groups={"Registration"})
      */
-    protected $description;
+    protected $name;
 
     /**
+     * @var array
+     *
+     * @ORM\Column(type="array")
+     */
+    protected $roles;
+
+    /**
+     * Constructor.
+     *
      * @param string $name
-     * @param array  $roles
+     * @param array $roles
      */
-    public function __construct($name = null, $roles = [])
+    public function __construct($name, $roles = array())
     {
-        parent::__construct($name, $roles);
+        $this
+            ->setName($name)
+            ->setRoles($roles);
     }
 
     /**
-     * @return string
+     * Returns ID.
+     *
+     * @return int
      */
-    public function getDescription()
+    public function getId()
     {
-        return $this->description;
+        return $this->id;
     }
 
+
     /**
-     * @param string $description
+     * Sets name.
+     *
+     * @param string $name
+     *
      * @return $this
      */
-    public function setDescription($description)
+    public function setName($name)
     {
-        $this->description = $description;
+        $this->name = $name;
 
         return $this;
     }
 
     /**
+     * Returns name.
+     *
      * @return string
      */
-    public function __toString()
+    public function getName()
     {
-        return (string) $this->getName();
+        return $this->name;
+    }
+
+    /**
+     * Sets roles.
+     *
+     * @param array $roles
+     *
+     * @return $this
+     */
+    public function setRoles(array $roles)
+    {
+        $this->roles = array();
+
+        foreach ($roles as $role) {
+            $this->addRole($role);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Adds role.
+     *
+     * @param string $role
+     *
+     * @return $this
+     */
+    public function addRole($role)
+    {
+        $role = (string) $role;
+        $role = strtoupper ($role);
+
+        if (!$this->hasRole($role)) {
+            $this->roles[] = $role;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Removes role.
+     *
+     * @param string $role
+     *
+     * @return $this
+     */
+    public function removeRole($role)
+    {
+        $role = (string) $role;
+
+        if (false !== $key = array_search(strtoupper($role), $this->roles, true)) {
+            unset($this->roles[$key]);
+            $this->roles = array_values($this->roles);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Returns roles.
+     *
+     * @return array
+     */
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    /**
+     * Returns true if user has role.
+     *
+     * @param string $role
+     *
+     * @return bool
+     */
+    public function hasRole($role)
+    {
+        $role = (string) $role;
+
+        return in_array(strtoupper($role), $this->roles, true);
     }
 }
