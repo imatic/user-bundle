4aca77a0eca48ba2e756f59f23d61d586db3c74b
Viliam Husár <viliam.husar@imatic.cz>
2013-09-02 10:21:01 +0200

    RESEARCH-12 - Úprava entit s použitím Interface. Pridana dokumentace, a providery.

---
+++ Security/UserProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Imatic\Bundle\UserBundle\Security;
+
+use FOS\UserBundle\Security\UserProvider as BaseUserProvider;
+use Imatic\Bundle\UserBundle\Entity\User;
+use Symfony\Component\Security\Core\User\UserInterface as SecurityUserInterface;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+
+/**
+ * Provides user by username.
+ *
+ * @author Viliam Husár <viliam.husar@imatic.cz>
+ */
+class UserProvider extends BaseUserProvider
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function refreshUser(SecurityUserInterface $user)
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Expected an instance of Imatic\Bundle\UserBundle\Entity\User, but got "%s".', get_class($user)));
+        }
+
+        if (!$this->supportsClass(get_class($user))) {
+            throw new UnsupportedUserException(sprintf('Expected an instance of %s, but got "%s".', $this->userManager->getClass(), get_class($user)));
+        }
+
+        if (null === $reloadedUser = $this->userManager->findUserBy(array('id' => $user->getId()))) {
+            throw new UsernameNotFoundException(sprintf('User with ID "%d" could not be reloaded.', $user->getId()));
+        }
+
+        return $reloadedUser;
+    }
+}
--- UPGRADE-2.3.md
+++ UPGRADE-2.3.md
@@ -1,18 +1,9 @@
-UPGRADE Z VERZE 2.2
-===================
+Konfigurace
+===========
+
+* Místo ``fos_user.user_provider.*`` používat ``imatic_user.user_provider.*``
 
 Entity
 ------
 
-Tahle verze může vyžadovat ručně vytvořenou migraci pro tabulky s uživateli a skupinami.
-Entity pro uživatele a s kupiny je třeba extendovat z ``Imatic\Bundle\UserBundle\Model\User``
-a ``Imatic\Bundle\UserBundle\Model\Group``. Zde je už nastaveno také mapování mezi ``User``
-a ``Group`` a musí být odebráno z aplikačních entít. Dále je nutno v ``app/config.yml`` nastavit:
-
-    doctrine:
-        orm:
-            resolve_target_entities:
-                Imatic\Bundle\UserBundle\Model\UserInterface: App\Bundle\UserBundle\Entity\User # resp. aplikační třída pro uživatele
-                Imatic\Bundle\UserBundle\Model\GroupInterface: App\Bundle\UserBundle\Entity\Group # resp. aplikační třída pro skupinu
-
-
+* Po přechode na na ``UserInterface`` pro stávajíci aplikace nastavit název tabulky přímo v entitě.
